986b3ae0e4fee9f2f6db8188a076f22afdd7c712
Joyce Martinez <joycetezanos@gmail.com>
2020-10-22 18:28:11 -0500

    Regions, categories and events routes created

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users
+  root to: 'pages#home'
+
   get 'providers', to: 'providers#index'
   get 'providers/new', to: 'providers#new'
   post 'providers', to: 'providers#create'
@@ -6,8 +9,11 @@ Rails.application.routes.draw do
   get 'providers/:id/edit', to: 'providers#edit'
   patch 'providers/:id', to: 'providers#update'
   delete 'providers/:id', to: 'providers#destroy'
-
-  devise_for :users
-  root to: 'pages#home'
+  get 'categories/new', to: 'categories#new'
+  post 'categories', to: 'categories#create'
+  get 'events/new', to: 'events#new'
+  post 'events', to: 'events#create'
+  get 'regions/new', to: 'regions#new'
+  post 'regions', to: 'regions#create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
