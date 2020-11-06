db1f76d03ee5013dc87527c4e89094952e65a241
Joyce Martinez <joycetezanos@gmail.com>
2020-11-05 19:03:50 -0600

    App and Blogs Controllers updated

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,15 +5,14 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!, :except => [:index, :show]
 
   # Pundit: white-list approach.
-  after_action :verify_authorized, except: [:index, :show], unless: :skip_pundit?
-  after_action :verify_policy_scoped, only: [:new, :update, :destroy], unless: :skip_pundit?
+  after_action :verify_authorized, :except => [:index, :show], unless: :skip_pundit?
 
   # Uncomment when you *really understand* Pundit!
-  # rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
-  # def user_not_authorized
-  #   flash[:alert] = "You are not authorized to perform this action."
-  #   redirect_to(root_path)
-  # end
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+  def user_not_authorized
+    flash[:alert] = "Necesitas permiso para realizar esta acción"
+    redirect_to(root_path)
+  end
 
   private
 
