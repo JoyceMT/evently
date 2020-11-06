7c52b39b47a4fd4e13c6b0f591530eaa69ac9fdc
Joyce Martinez <joycetezanos@gmail.com>
2020-11-05 19:00:21 -0600

    Blog model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_04_211312) do
+ActiveRecord::Schema.define(version: 2020_11_05_213732) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,15 @@ ActiveRecord::Schema.define(version: 2020_11_04_211312) do
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "provider_events", force: :cascade do |t|
@@ -127,6 +136,7 @@ ActiveRecord::Schema.define(version: 2020_11_04_211312) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "categories", "services"
+  add_foreign_key "posts", "users"
   add_foreign_key "provider_events", "events"
   add_foreign_key "provider_events", "providers"
   add_foreign_key "provider_regions", "providers"
