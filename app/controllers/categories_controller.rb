77b20d4c05beff0c38f64a19a6a7479c29ed79d0
Joyce Martinez <joycetezanos@gmail.com>
2021-03-24 21:25:41 -0600

    Services inside categories to create new provider

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,6 +3,11 @@ class CategoriesController < ApplicationController
     @categories = Category.all
   end
 
+  def show
+    @category = Category.find(params[:id])
+    @services = @category.services
+  end
+
   def new
     @category = Category.new
   end
